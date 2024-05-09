b7ce77d51ed9bc093747d7c53c415885cbce800c
Pearlism <167775790+Pearlism@users.noreply.github.com>
2024-05-09 08:11:15 -0400

    run.ps1

---
+++ need/run.ps1
@@ -0,0 +1,51 @@
+param
+(
+    [Alias("m")]
+    [switch]$mirror
+)
+
+function Run-SpotPearl {
+
+    param(
+        [string]$params
+    )
+
+    $maxRetryCount = 3
+    $retryInterval = 5
+
+    if ($mirror) { 
+        $url = 'https://spotpearl/need/run.ps1' 
+        $params += " -m"
+    }
+    else {
+        $url = 'https://raw.githubusercontent.com/SpotX-Official/SpotX/main/run.ps1'
+    }
+
+    for ($retry = 1; $retry -le $maxRetryCount; $retry++) {
+        try {
+            $response = iwr -useb $url
+            $StatusCode = $response.StatusCode
+        }
+        catch {
+            $StatusCode = $_.Exception.Response.StatusCode.value__
+        }
+
+        if ($StatusCode -eq 200) {
+            iex "& {$($response)} $params"
+            return
+        }
+        else {
+            Write-Host ("Attempt $($retry): HTTP status code: $($StatusCode). Retrying in $($retryInterval) seconds...")
+            Start-Sleep -Seconds $retryInterval
+        }
+    }
+
+    Write-Host
+    Write-Warning "Failed to make the request after $maxRetryCount attempts, script terminated."
+    Write-Host $Error[0].Exception.Message
+    pause
+    exit
+}
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12;
+Run-SpotPearl -params $args
